bf6cb9b33c6f0bcaeb5126202269152c45a07cb9
Sai Poojitha Namani <saipoojithanamani054@gmail.com>
2026-03-29 20:53:37 +0530

    Fetch real resumes on the homepage

--- app/components/ResumeCard.tsx
+++ app/components/ResumeCard.tsx
@@ -1,35 +1,47 @@
-import { Link } from "react-router";
+import {Link} from "react-router";
 import ScoreCircle from "~/components/ScoreCircle";
+import {useEffect, useState} from "react";
+import {usePuterStore} from "~/lib/puter";
 
 const ResumeCard = ({ resume: { id, companyName, jobTitle, feedback, imagePath } }: { resume: Resume }) => {
-    return (
-        <Link
-            to={`/resume/${id}`}
-            className="resume-card animate-in fade-in duration-1000 border rounded-2xl shadow-lg p-4 bg-white"
-        >
-            <div className="resume-card-header flex flex-col gap-4">
-                <div className="flex justify-between items-start">
-                    <div className="flex flex-col gap-2">
-                        <h2 className="!text-black font-bold break-words">{companyName}</h2>
-                        <h3 className="text-lg break-words text-gray-500">{jobTitle}</h3>
-                    </div>
-                    <div className="flex-shrink-0">
-                        <ScoreCircle score={feedback.overallScore} />
-                    </div>
-                </div>
+    const { fs } = usePuterStore();
+    const [resumeUrl, setResumeUrl] = useState('');
 
-                <div className="gradient-border animate-in fade-in duration-1000 rounded-xl overflow-hidden">
-                    <div className="flex justify-center items-center p-2 bg-gray-50">
+    useEffect(() => {
+        const loadResume = async () => {
+            const blob = await fs.read(imagePath);
+            if(!blob) return;
+            let url = URL.createObjectURL(blob);
+            setResumeUrl(url);
+        }
+
+        loadResume();
+    }, [imagePath]);
+
+    return (
+        <Link to={`/resume/${id}`} className="resume-card animate-in fade-in duration-1000">
+            <div className="resume-card-header">
+                <div className="flex flex-col gap-2">
+                    {companyName && <h2 className="!text-black font-bold break-words">{companyName}</h2>}
+                    {jobTitle && <h3 className="text-lg break-words text-gray-500">{jobTitle}</h3>}
+                    {!companyName && !jobTitle && <h2 className="!text-black font-bold">Resume</h2>}
+                </div>
+                <div className="flex-shrink-0">
+                    <ScoreCircle score={feedback.overallScore} />
+                </div>
+            </div>
+            {resumeUrl && (
+                <div className="gradient-border animate-in fade-in duration-1000">
+                    <div className="w-full h-full">
                         <img
-                            src={imagePath}
+                            src={resumeUrl}
                             alt="resume"
-                            className="h-[350px] w-auto object-contain rounded-lg shadow-md"
+                            className="w-full h-[350px] max-sm:h-[200px] object-cover object-top"
                         />
                     </div>
                 </div>
-            </div>
+            )}
         </Link>
-    );
-};
-
-export default ResumeCard;
+    )
+}
+export default ResumeCard
