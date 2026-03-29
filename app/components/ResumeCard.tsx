15d98fe5396b18ce4e51f4e3ad68c20271ee9df2
Sai Poojitha Namani <saipoojithanamani054@gmail.com>
2026-03-29 20:03:45 +0530

    Resume feedback components

--- app/components/ResumeCard.tsx
+++ app/components/ResumeCard.tsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router";
-import ScoreCircle from "~/components/scoreCircle";
+import ScoreCircle from "~/components/ScoreCircle";
 
 const ResumeCard = ({ resume: { id, companyName, jobTitle, feedback, imagePath } }: { resume: Resume }) => {
     return (
